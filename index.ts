d225f52576b7ade505610ac62def3d77d348a494
Angad Nadkarni <angad@gethullo.com>
2016-09-06 16:19:16 +0530

    Logging ID requested

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ app.get("/:id", (req, res) => {
   require("google-spreadsheet-to-json")({
     spreadsheetId: req.params.id
   }).then((json) => {
+    console.log("Serving document ID:", req.params.id);
     res.status(200).send(json);
   }).catch((err) => {
     res.status(500).send(err);
