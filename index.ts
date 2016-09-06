bf7ee855d3c787e3e73ccc04d857656d989e7eab
Angad Nadkarni <angad@gethullo.com>
2016-09-06 16:04:41 +0530

    First draft of Sheetson ready to test

---
+++ index.ts
@@ -0,0 +1,16 @@
+import * as express from "express";
+let app = express();
+
+app.get("/:id", (req, res) => {
+  require("google-spreadsheet-to-json")({
+    spreadsheetId: req.params.id
+  }).then((json) => {
+    res.status(200).send(json);
+  }).catch((err) => {
+    res.status(500).send(err);
+  });
+})
+
+app.listen(7000, () => {
+  console.log("sheetson on 7000. hon.");
+})
